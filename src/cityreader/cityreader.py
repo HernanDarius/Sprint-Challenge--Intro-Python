edb9cf182948a077c7d5f5a8361bc5e298586841
HernanDarius <hdmecheverry@gmail.com>
2020-09-04 19:53:24 -0400

    fixed cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -41,8 +41,7 @@ def cityreader(cities=[]):
       lat.append(row[3])
       lon.append(row[4])
 
-    merged_name_lat = [(name[i], lat[i]) for i in range(0, len(name))] 
-    merged_complete = [(merged_name_lat[i], lon[i]) for i in range(0, len(merged_name_lat))]
+    merged_complete = [(name[i], lat[i], lon[i]) for i in range(0, len(name))]
 
     for i in merged_complete:
       cities.append(City(i[0], i[1], i[2]))
